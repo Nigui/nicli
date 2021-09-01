37f8cddf8274538e9bd41341eb5c53ee4a24ead6
Guillaume Nicolas <guillaume.nicolas@spacelephant.org>
2021-09-01 19:44:10 +0200

    tmp: new share algorithm

--- src/blockchain/functions.ts
+++ src/blockchain/functions.ts
@@ -3,6 +3,7 @@ import { Transactions, Utils } from "@uns/ark-crypto";
 import {
   DELEGATE_ID,
   DELEGATE_PASSPHRASE,
+  DELEGATE_PUBLIC_KEY,
   VOTERS_BLACKLIST,
 } from "../constants";
 import { getNetworkTimestamp } from "./utils";
@@ -12,24 +13,139 @@ export const getVoters = (delegate: string): Promise<any[]> =>
     voters.filter(({ publicKey }: any) => !VOTERS_BLACKLIST.includes(publicKey))
   );
 
-type Share = { id: string; amount: number };
+type VoterIds = string[];
+type RewardShare = { id: string; amount: number }[];
+export type Period = { start: number; end: number };
 
-const computeShares = (voters: string[]): Share[] => {
-  const SECONDS_IN_A_MONTH = 3600 * 24 * 30;
-  const BLOCKS_IN_A_MONTH = SECONDS_IN_A_MONTH / 8;
-  console.log("BLOCKS_IN_A_MONTH", BLOCKS_IN_A_MONTH);
-  const BLOCK_FORGED_BY_DELEGATES_IN_A_MONTH = BLOCKS_IN_A_MONTH / 23;
-  const DELEGATE_REWARDS_IN_A_MONTH = BLOCK_FORGED_BY_DELEGATES_IN_A_MONTH * 2;
-  console.log("DELEGATE_REWARDS_IN_A_MONTH", DELEGATE_REWARDS_IN_A_MONTH);
+type VotersVotingStartDate = { id: string; startDateTS: number }[];
+const getVotingStartDate = async (
+  voters: VoterIds,
+  period: Period
+): Promise<VotersVotingStartDate> => {
+  const promises = voters.map(async (id: string) => {
+    // find all delegate vote transaction during period
+    const body = {
+      transactions: {
+        from: period.start,
+        to: period.end,
+      },
+      type: 5,
+      typeGroup: 2001,
+      senderPublicKey: id,
+      asset: {
+        votes: [`+${DELEGATE_PUBLIC_KEY}`],
+      },
+    };
+    const response = await post("v2/transactions/search", body);
+    return response.meta.totalCount > 0
+      ? { id, startDateTS: response.data[0].timestamp.epoch } // get last transaction date (must be ordered by date)
+      : { id, startDateTS: period.start };
+  });
+  return Promise.all(promises);
+};
 
-  const nbVoters = voters.length;
+type VotersVotingPercentage = { id: string; timePercent: number }[];
+const computeTimeSpentVoting = (
+  votersStartDate: VotersVotingStartDate,
+  period: Period
+): VotersVotingPercentage => {
+  return votersStartDate.map(({ id, startDateTS }) => {
+    return {
+      id,
+      timePercent: (startDateTS - period.start) / (period.end - period.start),
+    };
+  });
+};
 
-  const sharedReward = Math.floor(DELEGATE_REWARDS_IN_A_MONTH * 0.2);
+type VotersAvgStakedTokens = { id: string; amount: number }[];
+const computeAvgTokenStakedOnPeriod = (
+  votersStartDate: VotersVotingStartDate,
+  endDate: number
+): Promise<VotersAvgStakedTokens> => {
+  const promises = votersStartDate.map(async ({ id, startDateTS }) => {
+    const { balance, address } = await getWallet(id);
 
-  return voters.map((v) => ({
-    id: v,
-    amount: sharedReward / nbVoters,
-  }));
+    // get outcoming transaction
+    const response = await post("v2/transactions/search", {
+      transactions: {
+        from: startDateTS,
+        to: endDate,
+      },
+      senderPublicKey: id,
+    });
+    response.data.map(({ amount, fees }) => {});
+    // TODO handle multipayment transactions
+
+    // get incoming transactions
+    const responseIncoming = await post("v2/transactions/search", {
+      transactions: {
+        from: startDateTS,
+        to: endDate,
+      },
+      recipient: address,
+    });
+    // TODO handle multipayment transaction
+  });
+
+  return Promise.all(promises);
+};
+
+type VotersRewardPercentage = { id: string; rewardPercent: number }[];
+const computeVoterRewardPercentage = (
+  votingPercentages: VotersVotingPercentage,
+  avgTokenStakedOnPeriod: VotersAvgStakedTokens
+): VotersRewardPercentage => {
+  // TODO
+  return [];
+};
+
+const applyRewardPercentage = (
+  voterRewardPercentage: VotersRewardPercentage,
+  amountToShare: number
+): RewardShare => {
+  // TODO
+  return [];
+};
+
+export const computeRewardShares = async (
+  voters: VoterIds,
+  toShare: number,
+  periodDates: Period
+): Promise<RewardShare> => {
+  // const SECONDS_IN_A_MONTH = 3600 * 24 * 30;
+  // const BLOCKS_IN_A_MONTH = SECONDS_IN_A_MONTH / 8;
+  // console.log("BLOCKS_IN_A_MONTH", BLOCKS_IN_A_MONTH);
+  // const BLOCK_FORGED_BY_DELEGATES_IN_A_MONTH = BLOCKS_IN_A_MONTH / 23;
+  // const DELEGATE_REWARDS_IN_A_MONTH = BLOCK_FORGED_BY_DELEGATES_IN_A_MONTH * 2;
+  // console.log("DELEGATE_REWARDS_IN_A_MONTH", DELEGATE_REWARDS_IN_A_MONTH);
+
+  // const nbVoters = voters.length;
+
+  // const sharedReward = Math.floor(DELEGATE_REWARDS_IN_A_MONTH * 0.2);
+
+  // 1. Compute voting start date timestamp
+  const votingStartDate: VotersVotingStartDate = await getVotingStartDate(
+    voters,
+    periodDates
+  );
+
+  // 2. Compute percentage time spent voting during period
+  const votingPercentages: VotersVotingPercentage =
+    await computeTimeSpentVoting(votingStartDate, periodDates);
+
+  // 3. Compute average tokens staked on voting period
+  const avgTokenStakedOnPeriod: VotersAvgStakedTokens =
+    await computeAvgTokenStakedOnPeriod(votingStartDate, periodDates.end);
+
+  // 4. Compute voter reward percentage
+  const voterRewardPercentage: VotersRewardPercentage =
+    await computeVoterRewardPercentage(
+      votingPercentages,
+      avgTokenStakedOnPeriod
+    );
+
+  // 5. Apply reward percentage
+  return applyRewardPercentage(voterRewardPercentage, toShare);
 };
 
 const getNextWalletNonce = (walletId: string): Promise<any> =>
@@ -39,7 +155,7 @@ const getNextWalletNonce = (walletId: string): Promise<any> =>
       Utils.BigNumber.make(nonce).plus(1).toFixed();
     });
 
-const buildTransaction = async (shares: Share[]): Promise<any> => {
+const buildTransaction = async (shares: RewardShare): Promise<any> => {
   const nonce = await getNextWalletNonce(DELEGATE_ID);
   const builder = Transactions.BuilderFactory.multiPayment()
     .version(2)
@@ -55,16 +171,12 @@ const buildTransaction = async (shares: Share[]): Promise<any> => {
 
 export const computeForgedBlocks = async (
   delegateId: string,
-  fromDate: string,
-  toDate: string
+  period: Period
 ): Promise<number> => {
-  const fromTimestamp = await getNetworkTimestamp(fromDate);
-  const toTimestamp = await getNetworkTimestamp(toDate);
-
   const body = {
     timestamp: {
-      from: fromTimestamp,
-      to: toTimestamp,
+      from: period.start,
+      to: period.end,
     },
     generatorPublicKey: delegateId,
   };
--- src/commands/foo.ts
+++ src/commands/foo.ts
@@ -1,6 +1,12 @@
 import { Command } from "@oclif/command";
 import { cli } from "cli-ux";
-import { computeForgedBlocks, getVoters } from "../blockchain/functions";
+import {
+  computeForgedBlocks,
+  computeRewardShares,
+  getVoters,
+  Period,
+} from "../blockchain/functions";
+import { getNetworkTimestamp } from "../blockchain/utils";
 import { DELEGATE_ID, DELEGATE_PUBLIC_KEY } from "../constants";
 
 export default class Foo extends Command {
@@ -15,18 +21,28 @@ export default class Foo extends Command {
   async run() {
     const { args, flags } = this.parse(Foo);
 
-    const from = "2021-06-01T00:00:00.000Z";
-    const to = "2021-07-01T00:00:00.000Z";
+    const from = "2021-07-01T00:00:00.000Z";
+    const fromTS = await getNetworkTimestamp(from);
+    const to = "2021-08-01T00:00:00.000Z";
+    const toTS = await getNetworkTimestamp(to);
+
+    const period: Period = { start: fromTS, end: toTS };
+
     const delegate = DELEGATE_PUBLIC_KEY;
 
-    const num = await computeForgedBlocks(delegate, from, to);
+    const num = await computeForgedBlocks(delegate, period);
 
     const rewards = num * 2;
     const toShare = rewards * 0.2;
 
     const voters = await getVoters(DELEGATE_ID);
+
     const nbVoters = voters.length;
-    const rewardByVoter = Math.floor(toShare / voters.length);
+
+    // old formula
+    // const rewardByVoter = Math.floor(toShare / voters.length);
+
+    const rewardByVoter = await computeRewardShares(voters, toShare, period);
 
     this.log(`${delegate} forged ${num} blocks between ${from} and ${to}`);
     this.log(`and got ${rewards} UNIKs`);
